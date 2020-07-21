c5183dfeba1bef80685a6cb047f3d00119df5378
pl.hancharou@gmail.com <pl.hancharou@gmail.com>
2020-07-21 20:10:28 +0300

    create: add ymaps API for hotel-grid (hard)

--- js/main.js
+++ js/main.js
@@ -12,3 +12,41 @@ var mySwiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   },
 });
+
+ymaps.ready(init);
+
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [37.77229036108529, -122.4198695],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 14
+  }, {
+    searchControlProvider: 'yandex#search'
+  }),
+    // Создаем геообъект с типом геометрии "Точка".
+    myGeoObject = new ymaps.GeoObject({
+      // Описание геометрии.
+      geometry: {
+        type: "Point",
+      }
+    }, {
+      // Опции.
+      // Иконка метки будет растягиваться под размер ее содержимого.
+      preset: 'islands#blackStretchyIcon'
+    });
+
+  myMap.geoObjects
+    .add(myGeoObject)
+    .add(new ymaps.Placemark([37.77229036108529, -122.4198695], {
+      balloonContent: 'Hotel Hilton'
+    }, {
+      preset: 'islands#dotIcon',
+      iconColor: '#EC1F46'
+    }))
+}
